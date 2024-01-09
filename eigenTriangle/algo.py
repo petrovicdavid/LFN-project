e0401172b29d4d04f32ae9aa1122d79ef4c7b5a0
petrovicdavid <73351125+petrovicdavid@users.noreply.github.com>
2024-01-09 15:00:57 +0100

    Create algo.py (EigenTriangle)

---
+++ eigenTriangle/algo.py
@@ -0,0 +1,61 @@
+import numpy as np 
+import networkx as nx
+from scipy.sparse.linalg import eigsh
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import csgraph_from_dense
+
+
+def create_adjacency_matrix_sparse(edge_list):
+    G = nx.Graph()
+    G.add_edges_from(edge_list)
+    A = nx.to_scipy_sparse_array(G)
+    A = A.asfptype()
+    return A
+
+def condition(eigenvalues, i):
+   tol = 0.05
+   num = abs(eigenvalues[i-1]**3)
+   den = sum(x**3 for x in eigenvalues[:i])
+   value = num/den
+
+   if value >= 0 and value <= tol:
+      return False
+   else:
+      return True
+
+
+if __name__ == "__main__":
+    filepath = "fifth.txt"
+    #filepath = "input.txt"
+    separator = ' '
+    #separator = '\t'
+  
+    with open(filepath, 'r') as file:
+        edges_file = [tuple(map(int, line.strip().split(separator)[:2])) for line in file]
+
+
+    # NB:
+    # When applied to Hermitian matrices, the Arnoldi iteration 
+    # (the one used in scipy.sparse.linalg.eigs) reduces to the Lanczos algorithm.
+
+
+    A = create_adjacency_matrix_sparse(edges_file)
+
+    eigenvalues, vecs = eigsh(A, k = 50)
+
+    # the eigenvalues must be processed in reverse order considering them as their absolute value
+    # but they must be processed as they are (not absolute values)
+    eigenvalues_sorted = sorted(eigenvalues, key=abs, reverse=True)
+
+    n = len(eigenvalues_sorted)
+    #print(eigenvalues_sorted)
+
+    i = 3
+
+    while(condition(eigenvalues_sorted, i) and i < n):
+        i += 1
+
+    triangles = int(sum(x**3 for x in eigenvalues_sorted[:i])/6)
+
+    print(i)
+    print("Approximate number of triangles: " + str(triangles))
